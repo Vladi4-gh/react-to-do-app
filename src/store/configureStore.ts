4ce48bf32b74142c2e4c186c915c71d0cb7281cc
Vladislav Ivanov <vladi4.gm@gmail.com>
2021-03-31 19:27:58 +0300

    LocalStorage has been applied.

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,6 +1,6 @@
-import thunk from 'redux-thunk';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { save, load } from 'redux-localstorage-simple';
 import { appReducer } from './state/app/reducer';
 import { State } from './types/State';
 import { taskReducer } from './state/task/reducer';
@@ -9,6 +9,7 @@ const reducer = combineReducers<State>({
   app: appReducer,
   task: taskReducer
 });
-const enhancers = [applyMiddleware(thunk)];
+const localStorageNamespace = 'react-to-do-app';
+const storeEnhancers = [save({ namespace: localStorageNamespace })];
 
-export const store = createStore(reducer, composeWithDevTools(...enhancers));
+export const store = createStore(reducer, load({ namespace: localStorageNamespace }), composeWithDevTools(applyMiddleware(...storeEnhancers)));
